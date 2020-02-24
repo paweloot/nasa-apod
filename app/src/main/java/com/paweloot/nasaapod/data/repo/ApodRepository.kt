830e6b0a584f2c1ea0c106f4f6b6809d7662e7b9
Paweł Banaś <paweloot@gmail.com>
2020-02-24 10:15:13 +0100

    Add call for getting last week photos

--- app/src/main/java/com/paweloot/nasaapod/data/repo/ApodRepository.kt
+++ app/src/main/java/com/paweloot/nasaapod/data/repo/ApodRepository.kt
@@ -1,14 +1,31 @@
 package com.paweloot.nasaapod.data.repo
 
 import com.paweloot.nasaapod.BuildConfig
+import com.paweloot.nasaapod.data.model.Photo
 import com.paweloot.nasaapod.network.NasaApodApi
+import com.paweloot.nasaapod.util.DateUtils
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class ApodRepository(private val nasaApodApi: NasaApodApi) {
 
-    fun getPhotoForDate(date: String) =
+    fun getPhotoForDate(date: String): Single<Photo> =
         nasaApodApi.getPhotoForDate(date, BuildConfig.NASA_API_KEY)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+
+    fun getLastWeekPhotos(): Single<List<Photo>> {
+        val lastWeekDates = DateUtils.buildLastWeekDates()
+        val singles = mutableListOf<Single<Photo>>()
+
+        lastWeekDates.forEach { date ->
+            singles.add(getPhotoForDate(date))
+        }
+
+        return Single.merge(singles)
+            .toObservable()
+            .filter { it.mediaType == "image" }
+            .toList()
+    }
 }
