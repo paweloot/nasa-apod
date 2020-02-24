d257e9437eb07955f445bcb1562b5b34dc35eca6
Paweł Banaś <paweloot@gmail.com>
2020-02-24 13:34:29 +0100

    Add API call on init

--- app/src/main/java/com/paweloot/nasaapod/ui/MainViewModel.kt
+++ app/src/main/java/com/paweloot/nasaapod/ui/MainViewModel.kt
@@ -1,8 +1,45 @@
 package com.paweloot.nasaapod.ui
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import com.paweloot.nasaapod.data.model.Photo
+import com.paweloot.nasaapod.data.repo.ApodRepository
+import io.reactivex.rxkotlin.subscribeBy
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val apodRepository: ApodRepository) : DisposingViewModel() {
 
-    
+    data class ViewState(
+        val data: List<Photo> = listOf(),
+        val isLoading: Boolean = true,
+        val errorMsg: String = ""
+    )
+
+    val state = MutableLiveData<ViewState>().also { it.value = ViewState() }
+
+    init {
+        getPhotoList()
+    }
+
+    private fun getPhotoList() {
+        addDisposable(
+            apodRepository.getLastWeekPhotos()
+                .subscribeBy(
+                    onSuccess = this::onPhotosRetrieved,
+                    onError = this::onError
+                )
+        )
+    }
+
+    private fun onPhotosRetrieved(data: List<Photo>) {
+        state.value = state.value?.copy(
+            isLoading = false,
+            data = data
+        )
+    }
+
+    private fun onError(throwable: Throwable) {
+        state.value = state.value?.copy(
+            isLoading = false,
+            errorMsg = throwable.localizedMessage ?: ""
+        )
+    }
 }
